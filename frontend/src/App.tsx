122f068b3ce7f41be3a43d298201377d5408d5a8
Sam Robson <l28094@gmail.com>
2025-06-21 17:08:45 +0100

    fix: Remove unused useNavigate import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import './App.css'
-import { Routes, Route, useNavigate, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { PlayerProvider, usePlayer } from './PlayerContext'
 import Library from './Library'
 import ItemDetail from './ItemDetail'
